809b390bb7082a76a1e3621aa672ead69b0ac435
Dmitri <unabl4@gmail.com>
2019-04-17 12:15:08 +0300

    additional 'post' for the hello world rest api endpoint

--- src/test/java/hello/HelloWorldTest.java
+++ src/test/java/hello/HelloWorldTest.java
@@ -5,11 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -20,12 +22,18 @@ public class HelloWorldTest {
     private MockMvc mockMvc;
 
     @Test
-    public void testHello() throws Exception {
+    public void testHelloGet() throws Exception {
         MvcResult result = this.mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk()) // = 200
                 .andReturn();
 
         String content = result.getResponse().getContentAsString();
         assert content.equals("Hello World!");  // exact match
+    }
+
+    @Test
+    public void testHelloPost() throws Exception {
+        MvcResult result = this.mockMvc.perform(post("/hello")).andReturn();
+        assert result.getResponse().getStatus() == HttpStatus.METHOD_NOT_ALLOWED.value();
     }
 }
